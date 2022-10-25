16cec805e57910f31f174f9b3da2ea4aa773b653
krishosting <111237854+krishosting569@users.noreply.github.com>
2022-10-25 17:11:37 +0700

    Update mode-bot.js

--- plugins/mode-bot.js
+++ plugins/mode-bot.js
@@ -18,8 +18,8 @@ contextInfo: { externalAdReply :{
                         description: 'anu',
                         title: `💌 Ultah Owner : ${ohari} Hari ${ojam} Jam ${onet} Menit ${detek} Detik`,
                         body: `Subscribe YT My Bestie`,          previewType: 0,
-                        thumbnail: await (await fetch(`https://i.ibb.co/jfZVKmC/babi2.jpg`)).buffer(),
-                        sourceUrl: 'https://youtu.be/pwLZpdfO8AU'
+                        thumbnail: await (await fetch(`https://telegra.ph/file/35ab1cb0d5d4ff5c836f9.jpg`)).buffer(),
+                    'https://youtu.be/PeLEpmoiO4U'
                       }}
 })
 }
