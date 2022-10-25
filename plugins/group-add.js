a6aa930fcfc3b7596c3ad21a736c0173b4c01eee
krishosting <111237854+krishosting569@users.noreply.github.com>
2022-10-25 14:13:43 +0700

    Update group-add.js

--- plugins/group-add.js
+++ plugins/group-add.js
@@ -44,7 +44,7 @@ let handler = async (m, { conn, text, participants }) => {
     }
 }
 handler.help = ['add', '+'].map(v => v + ' @user')
-handler.tags = ['group']
+handler.tags = ['owner']
 handler.command = /^(add|\+)$/i
 
 handler.admin = true
