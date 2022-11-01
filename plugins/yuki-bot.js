1f8bd0fdcf4b34af84b06700d1ba4e263abfc99e
krishosting <111237854+krishosting569@users.noreply.github.com>
2022-11-01 22:43:33 +0700

    Update yuki-bot.js

--- plugins/yuki-bot.js
+++ plugins/yuki-bot.js
@@ -4,14 +4,14 @@ import fs from 'fs'
 let handler = async (m, { conn, command, text }) => {
   let name = await conn.getName(m.sender)
   let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-  let pp = await conn.profilePictureUrl(who, 'image').catch((_) => "https://telegra.ph/file/24fa902ead26340f3df2c.png")
+  let pp = await conn.profilePictureUrl(who, 'image').catch((_) => "https://telegra.ph/file/d70c18c4399bb0073179e.jpg")
 await conn.sendButton(m.chat, `Halo juga kak ${name} Ada Yang ${await conn.getName(conn.user.jid)} Bisa Bantu?\n`,wm + '\n\n' + botdate, giflogo2, [['MENU','.menu']], m, {
 contextInfo: { externalAdReply :{
                         mediaUrl: 'https://tiktok.com/@raraharsita2',
                         mediaType: 2,
                         description: 'anu',
                         title: global.titlebot,
-                        body: '⚘ ᴄʀᴇᴀᴛᴇ ʙʏ Kris Hosting',          previewType: 0,
+                        body: '⚘ ᴄʀᴇᴀᴛᴇ ʙʏ Xenon Store',          previewType: 0,
                         thumbnail: await (await fetch(pp)).buffer(),
                         sourceUrl: 'https://youtu.be/PeLEpmoiO4U'
                       }}
